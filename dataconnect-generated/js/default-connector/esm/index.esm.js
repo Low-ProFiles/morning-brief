7ebe69157d7162cb0074cced0802be2e963df69c
박민철 <parkmincheol@bagmincheol-ui-MacBookPro.local>
2025-05-25 16:43:11 +0900

    feat: firebase 연동 및 db 생성, 배포

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'morning-brief',
+  location: 'asia-northeast3'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'morning-brief',
+  location: 'asia-northeast3'
+};
+exports.connectorConfig = connectorConfig;
