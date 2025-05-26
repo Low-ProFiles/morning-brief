ff9434ddc31b0095e085c1748b0bceb01c2177a3
ddorimin <a54321y@naver.com>
2025-05-26 17:04:30 +0900

    App.tsx 및 설정 파일 수정, 새 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,35 @@ import  { useEffect, useState } from 'react'
 import { NewsItem } from './types/common/newsItem'
 import { useFetchNews } from './hooks/useFetchNews'
 import { NewsImportanceList } from './components/lists/newsImportanceList'
+import {  BrowserRouter, Route, Routes } from 'react-router-dom'
+import MyInfo from './pages/myInfo'
+import MyNews from './pages/myNews'
+import MyPage from './pages/myPage'
+import Notification from './pages/notification'
 
 export default function App() {
-  const [news, setNews] = useState<NewsItem[]>([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<string | null>(null)
+  // const [news, setNews] = useState<NewsItem[]>([])
+  // const [loading, setLoading] = useState(true)
+  // const [error, setError] = useState<string | null>(null)
 
-  useEffect(() => {
-    useFetchNews()
-      .then((items:any) => setNews(items))
-      .catch((e:any) => setError(e.message))
-      .finally(() => setLoading(false))
-  }, [])
+  // useEffect(() => {
+  //   useFetchNews()
+  //     .then((items:any) => setNews(items))
+  //     .catch((e:any) => setError(e.message))
+  //     .finally(() => setLoading(false))
+  // }, [])
 
-  if (loading) return <div>Loading news…</div>
-  if (error)   return <div style={{ color: 'red' }}>Error: {error}</div>
+  // if (loading) return <div>Loading news…</div>
+  // if (error)   return <div style={{ color: 'red' }}>Error: {error}</div>
 
   return (
-    <div>
-      <h1>뉴스 중요도</h1>
-      <NewsImportanceList news={news} />
-    </div>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/mypage' element={<MyPage />} />
+        <Route path='/mypage/info' element={<MyInfo />} />
+        <Route path='/mypage/news' element={<MyNews />} />
+        <Route path='/mypage/notification' element={<Notification />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
